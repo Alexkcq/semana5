ae3defea7aed56d61026339d62859336292a876e
Alexkcq <qalexander7858@gmail.com>
2024-09-12 08:21:01 -0500

    otro punto de salvado

--- semana5/Program.cs
+++ semana5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Hello, Word");
+string re = "s";
+double suma = 0;    
+double cant = 0;
+while (re == "s")
+{
+    Console.WriteLine("Desea participar (s/n)?");
+    re = Console.ReadLine();
+    Console.WriteLine("Ingrese edad");
+    int edad = Convert.ToInt32(Console.ReadLine());
+    suma = suma + edad;
+    cant = cant + 1;
+}
+double prom = suma / cant;
+Console.WriteLine($"EL promedio de {cant} edades es {prom}");
+Console.ReadKey();
